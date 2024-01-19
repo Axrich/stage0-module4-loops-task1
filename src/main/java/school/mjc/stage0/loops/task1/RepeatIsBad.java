27beee4bb1abdfc83b18f4dff26894cb630ca974
Axrich <116589250+Axrich@users.noreply.github.com>
2024-01-19 20:49:01 +0500

    Update RepeatIsBad.java

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,15 @@
 package school.mjc.stage0.loops.task1;
 
-public class RepeatIsBad {
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.repeatIsBad(20);
+    }
 
-    @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-
+    public void repeatIsBad(int times) {
+        if (times > 0) {
+            System.out.println("Writing the same code doesn't have much impact, and it's also time-consuming");
+            repeatIsBad(times - 1);
+        }
     }
 }
